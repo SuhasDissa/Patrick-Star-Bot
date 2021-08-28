c1e475fd07478f0fbe1c447d668acc2632003aa7
SuhasDissa <suhasdissa@gmail.com>
2021-08-28 06:54:45 +0530

    few performance improvements

--- commands/comic.js
+++ commands/comic.js
@@ -1,29 +1,55 @@
 const request = require("request");
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+comiclist = [];
 module.exports.run = async (bot, msg, args) => {
 
+
+  if (!comiclist.length) {
     try {
-        request("https://www.reddit.com/r/comics/top/.json?sort=top&t=week&limit=500", function(
-          error,
-          response,
-          body
-        ) {
-          if (!error && response.statusCode == 200) {
-            const comic= JSON.parse(body);
-            var i = getRndInteger(0,comic.data.children.length);
-            return msg.channel.send(comic.data.children[i].data.url);
+      request("https://www.reddit.com/r/comics/top/.json?sort=top&t=week&limit=500", function (
+        error,
+        response,
+        body
+      ) {
+        if (!error && response.statusCode == 200) {
+          const comic = JSON.parse(body);
+          var i = getRndInteger(0, comic.data.children.length);
+          for (x = 0; x < comic.data.children.length; x++) {
+            comiclist[x] = comic.data.children[x].data.url
           }
-        });
-      } catch (error) {
-        console.log(error);
-      }
+          return msg.channel.send(comic.data.children[i].data.url);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    var x = getRndInteger(0, comiclist.length);
+    return msg.channel.send(comiclist[x]);
+  }
+
+  /*
+      try {
+          request("https://www.reddit.com/r/comics/top/.json?sort=top&t=week&limit=500", function(
+            error,
+            response,
+            body
+          ) {
+            if (!error && response.statusCode == 200) {
+              const comic= JSON.parse(body);
+              var i = getRndInteger(0,comic.data.children.length);
+              return msg.channel.send(comic.data.children[i].data.url);
+            }
+          });
+        } catch (error) {
+          console.log(error);
+        }*/
 
 }
 
 module.exports.help = {
-    name: "comic",
-    aliases: ["cartoon"]
-} 
+  name: "comic",
+  aliases: ["cartoon"]
+}
--- commands/meme.js
+++ commands/meme.js
@@ -1,29 +1,57 @@
 const request = require("request");
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+memelist = [];
 
 module.exports.run = async (bot, msg, args) => {
 
+
+  if (!memelist.length) {
     try {
-        request("https://www.reddit.com/u/kerdaloo/m/dankmemer/top/.json?sort=top&t=week&limit=500", function(
-          error,
-          response,
-          body
-        ) {
-          if (!error && response.statusCode == 200) {
-            const comic= JSON.parse(body);
-            var i = getRndInteger(0,comic.data.children.length);
-            return msg.channel.send(comic.data.children[i].data.url);
+      request("https://www.reddit.com/r/memes/top/.json?sort=top&t=week&limit=500", function (
+        error,
+        response,
+        body
+      ) {
+        if (!error && response.statusCode == 200) {
+          const comic = JSON.parse(body);
+          var i = getRndInteger(0, comic.data.children.length);
+          for (x = 0; x < comic.data.children.length; x++) {
+            memelist[x] = comic.data.children[x].data.url
           }
-        });
-      } catch (error) {
-        console.log(error);
-      }
+          return msg.channel.send(comic.data.children[i].data.url);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    var x = getRndInteger(0, memelist.length);
+    return msg.channel.send(memelist[x]);
+  }
+
+
+  /* try {
+       request("https://www.reddit.com/u/kerdaloo/m/dankmemer/top/.json?sort=top&t=week&limit=500", function(
+         error,
+         response,
+         body
+       ) {
+         if (!error && response.statusCode == 200) {
+           const comic= JSON.parse(body);
+           var i = getRndInteger(0,comic.data.children.length);
+           return msg.channel.send(comic.data.children[i].data.url);
+         }
+       });
+     } catch (error) {
+       console.log(error);
+     }*/
 
 }
 
 module.exports.help = {
-    name: "meme",
-    aliases: ["memes"]
-} 
+  name: "meme",
+  aliases: ["memes"]
+}
