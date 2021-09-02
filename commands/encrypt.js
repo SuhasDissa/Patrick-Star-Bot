eb3b26c325ae18fd113db73499a9d8ab98a04a0e
SuhasDissa <suhasdissa@gmail.com>
2021-09-02 08:12:40 +0530

    Update encrypt.js

--- commands/encrypt.js
+++ commands/encrypt.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
-const key = 'okboomer';
+const key = '787ff81611b84c9ab2a55aa45e3c1d3e824e3ff583b0cb75c20b8947a4130d16';
 const iv = crypto.randomBytes(16);
 
 module.exports.run = async (bot, message, args) => {   
