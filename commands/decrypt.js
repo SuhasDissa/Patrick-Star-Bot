1a6f08d0fed011328306170b2d6e1a47148d3972
SuhasDissa <suhasdissa@gmail.com>
2021-09-02 16:07:57 +0530

    update

--- commands/decrypt.js
+++ commands/decrypt.js
@@ -1,8 +1,3 @@
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-const key = '787ff81611b84c9ab2a55aa45e3c1d3e824e3ff583b0cb75c20b8947a4130d16';
-const iv = key.substr(0, 16);
-
 module.exports.run = async (bot, message, args) => {   
 
     if (args.length < 1) {
@@ -11,17 +6,8 @@ module.exports.run = async (bot, message, args) => {
         );
     }
     var term= args.join(" ");
-
-function decrypt(text) {
- let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key.substr(0, 32)), iv);
- let decrypted = decipher.update(text);
- decrypted = Buffer.concat([decrypted, decipher.final()]);
- return decrypted.toString();
-}
-
 try {
-var hw = decrypt(String(term));
-console.log(term+","+hw);
+var hw = atob(term);
 return message.channel.send(hw);
 }catch(error){
     console.log(error);
--- commands/encrypt.js
+++ commands/encrypt.js
@@ -1,8 +1,3 @@
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-const key = '787ff81611b84c9ab2a55aa45e3c1d3e824e3ff583b0cb75c20b8947a4130d16';
-const iv = key.substr(0, 16);
-
 module.exports.run = async (bot, message, args) => {   
 
     if (args.length < 1) {
@@ -12,16 +7,8 @@ module.exports.run = async (bot, message, args) => {
     }
     var term= args.join(" ");
 
-function encrypt(text) {
- let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key.substr(0, 32)), iv);
- let encrypted = cipher.update(text);
- encrypted = Buffer.concat([encrypted, cipher.final()]);
- return encrypted.toString('hex');
-}
-
 try {
-var hw = encrypt(String(term));
-console.log(term+","+hw);
+var hw = btoa(term);
 return message.channel.send(hw);
 }catch(error){
     console.log(error);
