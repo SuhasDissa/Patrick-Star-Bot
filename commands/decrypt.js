23717c2260e28752f129d457739d0f50494ed741
SuhasDissa <suhasdissa@gmail.com>
2021-09-02 15:46:24 +0530

    update

--- commands/decrypt.js
+++ commands/decrypt.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
 const key = '787ff81611b84c9ab2a55aa45e3c1d3e824e3ff583b0cb75c20b8947a4130d16';
-const iv = crypto.randomBytes(16);
+const iv = key.substr(0, 16);
 
 module.exports.run = async (bot, message, args) => {   
 
@@ -13,7 +13,6 @@ module.exports.run = async (bot, message, args) => {
     var term= args.join(" ");
 
 function decrypt(text) {
- let iv = Buffer.from(text.iv, 'hex');
  let encryptedText = Buffer.from(text.encryptedData, 'hex');
  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key.substr(0, 32)), iv);
  let decrypted = decipher.update(encryptedText);
--- commands/encrypt.js
+++ commands/encrypt.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
 const key = '787ff81611b84c9ab2a55aa45e3c1d3e824e3ff583b0cb75c20b8947a4130d16';
-const iv = crypto.randomBytes(16);
+const iv = key.substr(0, 16);
 
 module.exports.run = async (bot, message, args) => {   
 
