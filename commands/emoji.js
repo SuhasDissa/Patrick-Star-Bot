4c36ea5b50562db92bc104c3a3c5cfef67e562dc
SuhasDissa <suhasdissa@gmail.com>
2021-09-04 12:07:36 +0530

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,16 +3,63 @@ const values = require("../variables.js");
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-  }
-  
+}
+var namelist = [];
+var urllist = [];
+
+function fillArray(comic) {
+    namelist.push(comic.title);
+    urllist.push(comic.image);
+}
 module.exports.run = async (bot, msg, args) => {
 
-    var i = getRndInteger(0, values.emoji.length);
-    return msg.channel.send(values.emoji[i]);
+    if (args.length < 1) {
+        return msg.channel.send(
+            "Tell me a keyword you nigge"
+        );
+    }
+    var filter = args[0].toUpperCase();
 
+    if (!namelist.length) {
+        try {
+            request("http://emoji.gg/api/", function (
+                error,
+                response,
+                body
+            ) {
+                if (!error && response.statusCode == 200) {
+
+                    const comic = JSON.parse(body);
+
+                    comic.forEach(fillArray);
+
+                    for (i = 0; i < namelist.length; i++) {
+                        txtValue = namelist[i];
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+
+                            return msg.channel.send(namelist[i] + ": /n" + urllist[i]);
+                        }
+                    }
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        try {
+            for (i = 0; i < namelist.length; i++) {
+                txtValue = namelist[i];
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    return msg.channel.send(namelist[i] + ": /n" + urllist[i]);
+                }
+            }
+        } catch (error) {
+
+        }
+    }
 }
 
 module.exports.help = {
     name: "emoji",
-    aliases: ["mood"]
-} 
+    aliases: ["sticker"]
+}
