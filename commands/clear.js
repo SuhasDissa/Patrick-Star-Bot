14d70d90f20efb0d99d8f3e9ee8831c2467bcf4c
SuhasDissa <suhasdissa@gmail.com>
2021-09-24 12:41:02 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 if (!amount) return message.reply('Deleted nothing cos you didnt ask me to');
 if (isNaN(amount)) return message.reply('Stop fucking around');
 
-if (amount > 1000) return message.reply('You can`t delete more than 1000 messages at once!');
+if (amount > 100) return message.reply('You can\'t delete more than 100 messages at once! (tbh Discord doesen\'t let me)');
 if (amount < 1) return message.reply('Are you nuts?');
 
 await message.channel.messages.fetch({ limit: amount }).then(messages => {
